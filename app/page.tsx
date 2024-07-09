86ad51f643ae00ad36cc1c4cc68a84a3bf7a8080
Jake Ducati <107895867+JakeDucati@users.noreply.github.com>
2024-07-09 10:20:45 -0700

    CSS & React
    - fixed list item component
    - fixed and added some CSS (Tailwind)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 
 
-// list item
-function ListItem(itemId, itemName) {
-    return ( // change color slightly when hovering
-        <div className="flex justify-between">
-            <div>
-                <Image src={"/thumbs/" + {itemId}} width={40} height={40} alt="Image of {itemName}" className="rounded-full" />
+const ListItem: React.FC<{ itemId: string; itemName: string }> = ({ itemId, itemName }) => {
+    return (
+        <div className="flex justify-between items-center p-2 hover:bg-slate-800 duration-200 rounded cursor-pointer">
+            <div className="flex items-center">
+                <Image src={"/thumbs/" + {itemId}} width={40} height={40} alt="Thumbnail" className="rounded-full mr-4" />
                 <div>{itemName}</div>
             </div>
             <button>
@@ -14,29 +13,27 @@ function ListItem(itemId, itemName) {
             </button>
         </div>
     );
-}
+};
 
 
 // page
 export default function Home() {
     return (
-        <div id="app">
-            <div className="header flex justify-between items-center p-6 m-4 rounded-xl bg-slate-950">
+        <div id="app" className="h-screen flex flex-col">
+            <div className="p-6 mt-4 ml-4 mr-4 rounded-xl bg-slate-950">
                 <h1 className="text-2xl">Stuff-Base</h1>
-                <div className="flex items-center">
-                    <span>
-                        <input type="text" id="searchBox" placeholder="Search" className="outline-none bg-transparent border-b-2 border-slate-50" />
-                        <button className="-ml-6">
-                            <span className="material-symbols-outlined">search</span>
-                        </button>
-                    </span>
-                    <button className="text-6xl ml-8">+</button>
-                </div>
             </div>
-
-            <div className="p-6 m-4 rounded-xl bg-slate-950">
-                <h2 className="text-xl">Stuff</h2>
-                <ListItem />
+            <div className="flex-grow p-6 m-4 rounded-xl bg-slate-950 flex flex-col overflow-hidden">
+                <div className="flex justify-between items-center mb-6">
+                    <h2 className="text-xl">Stuff</h2>
+                    <div className="flex items-center">
+                        <input type="text" id="searchBox" placeholder="Search" className="outline-none bg-transparent border-b-2 border-slate-50" />
+                        <button className="text-6xl ml-8">+</button>
+                    </div>
+                </div>
+                <div className="flex-grow overflow-y-auto flex flex-col">
+                    <ListItem itemId="73786325786" itemName="Fridge" />
+                </div>
             </div>
         </div>
     );
